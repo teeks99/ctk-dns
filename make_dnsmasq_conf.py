5cf1562d8b0614278b62426e653db69167631d44
Thomas Kent <teeks99@yahoo.com>
2016-06-11 07:34:06 -0500

    Fixes config
    The 0.0.0.0 address doesn't appear to work for listening, so use the auto-detected one.
    The cname stuff didn't work out, so switching back to address records.

--- make_dnsmasq_conf.py
+++ make_dnsmasq_conf.py
@@ -1,11 +1,17 @@
 import sys
 
-REDIRECT="interceptor"
+import local
 
 with open(sys.argv[1], "r") as domains:
     with open(sys.argv[2], "w") as conf:
         for domain in domains:
             domain_no_cr = domain[:-1]
-            #conf.write("address=/." + domain_no_cr + "/" + REDIRECT + "\n")
-            conf.write("cname=." + domain_no_cr + "," + REDIRECT + "\n")
+
+            conf.write("address=/." + domain_no_cr + "/" +
+                local.REDIRECT + "\n")
+
+            # The cname option doesn't work with domains with a dot in front, 
+            # so it only applies to the exact domain, instead of any
+            # subdomains
+            #conf.write("cname=." + domain_no_cr + "," + REDIRECT + "\n")
 
--- setup_onetime.bash
+++ setup_onetime.bash
@@ -2,10 +2,13 @@
 # This script will configure the system as a DNS filter
 
 # Install dnsmasq DNS caching server and apach2 web server
-sudo apt-get -y install dnsmasq apache2
+sudo apt-get -y install dnsmasq apache2 dnsutils
+
+# Automatically use the address that has the route out (test with google's DNS server)
+listen_address=`ip route get 8.8.8.8 | head -1 | cut -d' ' -f8`
 
 # Load the settings for dnsmasq
-sudo cp conf/dnsmasq_settings.conf /etc/dnsmasq.d/
+sudo sed s/LISTEN_ADDRESS/$listen_address/g conf/dnsmasq_settings.conf > /etc/dnsmasq.d/dnsmasq_settings.conf
 
 # Support google safe search
 sudo cp conf/safe_search.conf /etc/dnsmasq.d/
@@ -13,9 +16,8 @@ sudo cp conf/safe_search.conf /etc/dnsmasq.d/
 # Set a landing page for intercepted queries
 sudo cp conf/index.html /var/www/html/
 
-# Set a line in the hosts file to handle intercepted queries, pointed to the public IP of this computer
-sudo echo \# The following is the line that sets where rejected DNS queries should go >> /etc/hosts
-sudo echo `ip route get 8.8.8.8 | head -1 | cut -d' ' -f8`      interceptor >> /etc/hosts
+echo \# Python Settings for the local builder > local.py
+echo REDIRECT=$listen_address >> local.py
 
 script_dir=`pwd`
 pushd /etc/dnsmasq.d/
