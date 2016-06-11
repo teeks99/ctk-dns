4ca2e857913d999b3c963f42808d3edfc71d1a75
Thomas Kent <teeks99@yahoo.com>
2016-06-11 07:36:42 -0500

    Restarting server after update

--- update.bash
+++ update.bash
@@ -5,3 +5,5 @@ wget ftp://ftp.ut-capitole.fr/pub/reseau/cache/squidguard_contrib/adult.tar.gz -
 tar -xf downloads/adult.tar.gz
 python make_dnsmasq_conf.py adult/domains adult/adult.conf
 
+
+sudo service dnsmasq restart
