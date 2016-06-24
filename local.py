ef68e11cd62233ec664c4dd447feb394cee2d196
Thomas Kent <teeks99@yahoo.com>
2016-06-23 20:58:03 -0500

    Added a template of the required local.py file

---
+++ local.py
@@ -0,0 +1,7 @@
+# The following line should be set on a per-machine basis. This is the address
+# that the DNS request will redirect to, it is recommended that a http server
+# be running here, so that a landing page can be served instead of the blocked
+# content.
+
+#REDIRECT="10.53.1.13"
+
